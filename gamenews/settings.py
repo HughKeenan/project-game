df339e3eee0dec055010b9323cd87c2ca18fe82c
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-07 19:50:15 +0000

    add secret key

--- gamenews/settings.py
+++ gamenews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jeenfr3(kjgo-tpo=7+u3$-2(aoun20^z&ng3d40wp8s4#70v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
