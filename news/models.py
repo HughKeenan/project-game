149df61a64911a284e9672cf58659986c559b70d
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-07 20:14:08 +0000

    create Thread model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Thread(models.Model):
+    title = models.CharField(max_length=150, unique = True)
+    slug = models.SlugField(max_length=200, unique=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_posts")
+    body = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
