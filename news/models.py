5f7214c5f4ff50da44c95ed6f2903811bfb6e618
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-09 16:17:37 +0000

    update to Thread & Response models

--- news/models.py
+++ news/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+VISIBLE = ((0, "Visible"), (1, "Invisible"))
+
 # Create your models here.
 """
 Stores a thread post related to :model `auth.User`
 """
 class Thread(models.Model):
-    title = models.CharField(max_length=150, unique = True)
+    title = models.CharField(max_length=150, unique = True, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
     poster = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="news_posts")
     body = models.TextField()
     posted_on = models.DateTimeField(auto_now_add=True)
+    visible = models.IntegerField(choices=VISIBLE, default=0)
     class Meta:
         ordering = ["-posted_on"]
     def __str__(self):
@@ -20,10 +23,11 @@ class Response(models.Model):
     """
     Stores a reponse post related to :model `auth.Thread`
     """
-    thread = models.ForeignKey(Thread, on_delete=models.RESTRICT, related_name="responses")
     poster = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="responder")
+    thread = models.ForeignKey(Thread, on_delete=models.RESTRICT, related_name="responses")
     body = models.TextField()
     posted_on = models.DateTimeField(auto_now_add=True)
+    visible = models.IntegerField(choices=VISIBLE, default=0)
     class Meta:
         ordering = ["-posted_on"]
     def __str__(self):
