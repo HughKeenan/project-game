2641131cd154eee4587baebfcbb5e7935fbb471c
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-09 15:26:56 +0000

    update to wireframes & flowchart

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.ThreadList.as_view(), name='homepage'),
+    path('', views.ThreadList.as_view(), name='threadlist'),
 ]
