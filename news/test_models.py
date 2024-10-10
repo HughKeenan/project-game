081e41f3f472187d02356c7513d9ca8e5a9519f8
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-10 14:44:45 +0000

    implement sqlite tests for Thread & Response str() methods

--- news/test_models.py
+++ news/test_models.py
@@ -1,25 +1,6 @@
 from django.test import TestCase
-from .models import Thread
+from .models import Thread, Response
 from django.contrib.auth.models import User
-
-
-# class TestThread(TestCase):
-
-#     self.user = User.objects.create(
-#         username = 'JohnD',
-#     )
-
-#     self.model = Thread.objects.create(
-#         title='Test thread',
-#         slug='test-thread',
-#         poster= self.user, created = User.objects.get_or_create(username="JohnD"),
-#         body='This is a test thread',
-#         posted_on='10/10/2024',
-#         visible=0,
-#     )    
-
-#     def test_model_is_valid(self):
-#         self.assertEqual(self.model.has_body(), True)
 
 
 class TestThread(TestCase):
@@ -35,6 +16,43 @@ class TestThread(TestCase):
             posted_on='2024-10-10',
             visible=0,
         )
+        
 
-    def test_model_is_valid(self):
-        self.assertTrue(self.model.has_body())
+    def test_thread_str(self):
+        """Test the sales string representation."""
+        self.assertEqual(
+            (
+                str(self.model.body)
+            ),
+            'This is a test thread'
+        )
+
+class TestResponse(TestCase):
+
+    def setUp(self):
+        self.user, created = User.objects.get_or_create(username='JohnD')
+        self.thread, created = Thread.objects.get_or_create(
+            title='Test thread',
+            slug='test-thread',
+            poster=self.user,
+            body='This is a test thread',
+            posted_on='2024-10-10',
+            visible=0,)
+
+        self.model = Response.objects.create(
+            poster=self.user,
+            thread = self.thread,
+            body='This is a test response',
+            posted_on='2024-10-10',
+            visible=0,
+        )
+        
+
+    def test_response_str(self):
+        """Test the sales string representation."""
+        self.assertEqual(
+            (
+                str(self.model.body)
+            ),
+            'This is a test response'
+        )        
