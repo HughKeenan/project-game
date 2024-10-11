4c13edf2cd079e9403c85685fca90b0ba68e7c1c
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-11 10:42:55 +0000

    create thread_detail.html

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Thread
 
@@ -10,3 +10,26 @@ class ThreadList(generic.ListView):
     queryset = Thread.objects.all()
     template_name = "news/index.html"
     paginate_by = 1
+
+def thread_detail(request, slug):
+    """
+    Display an individual :model:`news.Thread`.
+
+    **Context**
+
+    ``thread``
+        An instance of :model:`news.Thread`.
+
+    **Template:**
+
+    :template:`news/thread_detail.html`
+    """
+
+    queryset = Thread.objects.filter(status=0)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "news/thread_detail.html",
+        {"thread": thread},
+    )
