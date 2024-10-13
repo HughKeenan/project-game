39be6006621584974b0db874330dbd139679634f
Hugh Keenan <hugh.keenan114@gmail.com>
2024-10-13 17:25:54 +0000

    add authentication for users

--- gamenews/urls.py
+++ gamenews/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("report/", include("report.urls"), name="report-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("", include("news.urls"), name="news-urls"),
     path('summernote/', include('django_summernote.urls')),
