cc740f35c3137abea40d3f8538853222138f0231
Hugh Keenan <hugh.keenan114@gmail.com>
2024-11-02 16:47:19 +0000

    add about us link

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,7 @@ url patterns for news app
 urlpatterns = [
     path('', views.ThreadList.as_view(), name='threadlist'),
     path('<slug:slug>/', views.thread_detail, name='thread_detail'),
+    path('welcome-to-the-site/', views.thread_detail, name='about_us'),
     path('<slug:slug>/edit_response/<int:response_id>',
          views.response_edit, name='response_edit'),
     path('<slug:slug>/delete_response/<int:response_id>',
